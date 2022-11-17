91aa3e1d24e4655ef1c7f059678a71583c79b311
xfkoko <kosti.korhonen@tuni.fi>
2022-11-17 21:31:05 +0200

    Place arrows

--- script.js
+++ script.js
@@ -4,12 +4,47 @@ window.addEventListener('load', function() {
     const CANVAS_WIDTH = canvas.width = 3250;
     const CANVAS_HEIGHT = canvas.height = 1300;
 
+    var rect = {
+        x:0,
+        y:0,
+        width:200,
+        height:100,
+
+    }
+
+    const rightArrow = new Image();
+    rightArrow.src = 'arrow_frames/rightpassive.png';
+    const leftArrow = new Image();
+    leftArrow.src = 'arrow_frames/leftpassive.png';
+
+    canvas.addEventListener('click', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+    
+        if (isInside(mousePos,rect)) {
+            alert('clicked inside rect');
+        }else{
+            alert('clicked outside rect');
+        }   
+    }, false);
+
     const textDisplay = this.document.getElementById('textDisplay');
     const rButton = this.document.getElementById('rightButton');
     const lButton = this.document.getElementById('leftButton');
 
     rButton.addEventListener('click', rightClicked);
     lButton.addEventListener('click', leftClicked);
+
+    function getMousePos(canvas, event) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top
+        };
+    }
+
+    function isInside(pos, rect) {
+        return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y;
+    }
 
     function rightClicked() {
         textDisplay.innerHTML = "Right clicked";
@@ -34,6 +69,8 @@ window.addEventListener('load', function() {
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         let position = Math.floor(gameFrame/staggerFrames) % 18;
         frameX = spriteWidth * position;
+        ctx.drawImage(rightArrow, 1675, 880);
+        ctx.drawImage(leftArrow, 1168, 880);
         ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth,
             spriteHeight, 50, 500, spriteWidth, spriteHeight);
 
