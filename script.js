0b793d166430ed02e55171419885ce4ba48b2716
xfkoko <kosti.korhonen@tuni.fi>
2023-05-03 23:18:28 +0300

    Change abse path to AWS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const BASE_PATH = "http://localhost:3000";
-//const BASE_PATH = "http://13.53.123.247:3000";
+//const BASE_PATH = "http://localhost:3000";
+const BASE_PATH = "http://13.51.174.58:3000";
 
 async function postScore(score, name) {
     console.log(name + ":", score);
